12f40cfaab81f8b59ce1962caecc73b8fe8513d1
Oleksii Sakun <leha.sakun@gmail.com>
2021-04-29 16:03:45 +0300

    add deletion

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,12 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
 
 export default function Card(props) {
-  const dispatch = useDispatch();
-
-  function handleDeleteTask(event) {
-    console.log(event);
-  }
-
   return (
     <div className="card">
       <input type="checkbox" />
       {props.task}
-      <a
-        onClick={handleDeleteTask}
-        href="#"
+      <button
+        onClick={props.handleDeleteTask}
         className="checklist_task-remove"
       />
     </div>
