0f04fb15f814b10dc168062df068520841f15f4a
Oleksii Sakun <leha.sakun@gmail.com>
2021-04-29 17:51:43 +0300

    not-working-move-to-in-progress

--- src/redux/actions/inputAction.js
+++ src/redux/actions/inputAction.js
@@ -1,4 +1,8 @@
-import { CREATE_TASK, DELETE_TASK } from "../constants";
+import {
+  CREATE_TASK,
+  DELETE_TASK,
+  MOVE_TASK_TO_IN_PROGRESS,
+} from "../constants";
 
 export const addTask = (task, id) => ({
   type: CREATE_TASK,
@@ -7,5 +11,10 @@ export const addTask = (task, id) => ({
 
 export const deleteTask = (task) => ({
   type: DELETE_TASK,
+  payload: task,
+});
+
+export const moveTaskToInProgress = (task) => ({
+  type: MOVE_TASK_TO_IN_PROGRESS,
   payload: task,
 });
